4861a6cfbe952e84af5ac74f8ddbc09389b0c106
etienne <squawkcpp@gmail.com>
2018-04-07 23:41:17 +0200

    add documentation and codec options handling

---
+++ docs/search/variables_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['title',['title',['../struct_cuesheet_item.html#aeeaa002dc7ba117a58784c3affb9a28f',1,'CuesheetItem::title()'],['../struct_cuesheet.html#a0ec81439b188d4a38173e22cc063a19c',1,'Cuesheet::title()']]],
+  ['track',['track',['../struct_cuesheet_item.html#abe916049589573be9c3bcaed78d400db',1,'CuesheetItem']]],
+  ['tracks',['tracks',['../struct_cuesheet.html#abb1535c01b6bd9f269434e4ba3b7071f',1,'Cuesheet']]],
+  ['type',['type',['../struct_cuesheet_item.html#ad1dea8ce88520878c48bd8bcdf6b9ec6',1,'CuesheetItem::type()'],['../struct_cuesheet.html#aeaf38dc063b000e9ab53e461e7e8537a',1,'Cuesheet::type()']]]
+];
