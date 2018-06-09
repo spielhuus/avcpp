8aff346fce0b058fa4890e8f66332dc2f3bb8a74
etienne <squawkcpp@gmail.com>
2018-06-10 00:21:14 +0200

    update buildfile

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ IMAGE=spielhuus/toolchain:1.0.1
 PID=$(sudo docker run -itd -v $(pwd):/repo -v $(pwd)/.build:/.build $IMAGE /bin/sh)
 
 echo "build avcpp version: $2"
-DOCKER_EXEC="sudo docker exec $PID /bin/sh -c"
+DOCKER_EXEC="sudo docker exec $PID /bin/sh -c "
 $DOCKER_EXEC "conan user -p $1 -r conan-cpp squawkcpp"
 $DOCKER_EXEC "cd /repo && \
     conan export . conan-cpp/latest && \
