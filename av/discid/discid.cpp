1a17cc972fad18e6a65f3b03d70566e2ef509f88
etienne <squawkcpp@gmail.com>
2018-08-05 13:21:32 +0200

    add libcurl

--- av/discid/discid.cpp
+++ av/discid/discid.cpp
@@ -25,7 +25,8 @@
 #include "chromaprint.h"
 
 #include "../av.h"
-#include "http/httpclient.h"
+// #include "http/httpclient.h"
+#include <curl/curl.h>
 
 #include "musicbrainz.h"
 #include "cddb.h"
@@ -43,12 +44,98 @@ std::ostream& operator<< ( std::ostream& stream, release_t& r ) {
     return stream;
 }
 
+struct MemoryStruct {
+    char *memory;
+    size_t size;
+};
+
+static size_t
+WriteMemoryCallback ( void *contents, size_t size, size_t nmemb, void *userp ) {
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = ( struct MemoryStruct * ) userp;
+
+    mem->memory = reinterpret_cast< char* > ( realloc ( mem->memory, mem->size + realsize + 1 ) );
+
+    if ( mem->memory == NULL ) {
+        /* out of memory! */
+        printf ( "not enough memory (realloc returned NULL)\n" );
+        return 0;
+    }
+
+    memcpy ( & ( mem->memory[mem->size] ), contents, realsize );
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
 std::error_code get ( const std::string& uri, std::stringstream& ss ) {
 
-    auto _response = http::get ( uri, ss );
+    CURL *curl_handle;
+    CURLcode res;
 
-    if ( _response.status() != http::http_status::OK )
-    { return av::make_error_code ( static_cast< int > ( _response.status() ) ); }
+    struct MemoryStruct chunk;
+
+    chunk.memory = reinterpret_cast< char* > ( malloc ( 1 ) ); /* will be grown as needed by the realloc above */
+    chunk.size = 0;    /* no data at this point */
+
+    curl_global_init ( CURL_GLOBAL_ALL );
+
+    /* init the curl session */
+    curl_handle = curl_easy_init();
+
+    /* specify URL to get */
+    curl_easy_setopt ( curl_handle, CURLOPT_URL, uri.c_str() );
+
+    /* send all data to this function  */
+    curl_easy_setopt ( curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback );
+
+    /* we pass our 'chunk' struct to the callback function */
+    curl_easy_setopt ( curl_handle, CURLOPT_WRITEDATA, ( void * ) &chunk );
+
+    /* some servers don't like requests that are made without a user-agent
+       field, so we provide one */
+    curl_easy_setopt ( curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0" );
+
+    /* get it! */
+    res = curl_easy_perform ( curl_handle );
+
+    /* check for errors */
+    if ( res != CURLE_OK ) {
+        fprintf ( stderr, "curl_easy_perform() failed: %s\n",
+                  curl_easy_strerror ( res ) );
+    }
+
+    else {
+        /*
+         * Now, our chunk.memory points to a memory block that is chunk.size
+         * bytes big and contains the remote file.
+         *
+         * Do something nice with it!
+         */
+
+        printf ( "%lu bytes retrieved\n", ( unsigned long ) chunk.size );
+        std::cout << std::string ( chunk.memory, 0, chunk.size ) << std::endl;
+        ss << std::string ( chunk.memory, 0, chunk.size );
+    }
+
+    /* cleanup curl stuff */
+    curl_easy_cleanup ( curl_handle );
+
+    free ( chunk.memory );
+
+    /* we're done with libcurl, so clean it up */
+    curl_global_cleanup();
+
+
+
+
+
+
+//    auto _response = http::get ( uri, ss );
+
+//    if ( _response.status() != http::http_status::OK )
+//    { return av::make_error_code ( static_cast< int > ( _response.status() ) ); }
 
     return std::error_code();
 }
